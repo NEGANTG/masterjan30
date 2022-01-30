4c2972fe68abc77d424de444c58094d93677b4b8
NEGANTG <90514589+NEGANTG@users.noreply.github.com>
2022-01-30 10:26:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/NEGANTG/masterjan30.git /masterjan30
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /masterjan30
 fi
-cd /EvaMaria
+cd /masterjan30
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
