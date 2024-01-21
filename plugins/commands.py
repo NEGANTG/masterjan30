33531f634eaab0f259c744f891a3adbd13f768cd
Nikhil Eashy P <70377480+nikhileashy@users.noreply.github.com>
2024-01-21 05:07:31 -0800

    ok ok

--- plugins/commands.py
+++ plugins/commands.py
@@ -203,7 +203,7 @@ async def start(client, message):
 
     files_ = await get_file_details(file_id)           
     if not files_:
-        pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("utf-8")).split("_", 1)
+        pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
         try:
             msg = await client.send_cached_media(
                 chat_id=message.from_user.id,
